bd9fa606dd81f50e571b5137297624c861c4a7be
Oka Kun <oka.sentana@taksu.tech>
2024-03-28 21:44:27 +0800

    fixed hitung mundur

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { fade, fly } from "svelte/transition";
+  import { onMount, onDestroy } from "svelte";
   import Saos from "saos";
   let isOpenContent = false;
   let isLoading = true;
@@ -8,6 +9,38 @@
     console.log("toggleContent");
     isOpenContent = !isOpenContent;
   };
+
+  let weddingDate = new Date("2024-04-14").getTime(); // Tanggal pernikahan
+  let now = new Date().getTime(); // Waktu saat ini
+  let timeLeft = weddingDate - now; // Selisih waktu antara sekarang dan tanggal pernikahan
+
+  let daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let hoursLeft = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  const updateCountdown = () => {
+    now = new Date().getTime();
+    timeLeft = weddingDate - now;
+    daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    hoursLeft = Math.floor(
+      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  };
+
+  const interval = setInterval(updateCountdown, 1000);
+
+  onMount(() => {
+    updateCountdown(); // Update countdown saat komponen dimount
+  });
+
+  onDestroy(() => {
+    clearInterval(interval); // Membersihkan interval saat komponen dihancurkan
+  });
 </script>
 
 <svelte:head>
@@ -60,7 +93,7 @@
           <div class="text-center text-white">
             <h4>The Wedding Of</h4>
             <h1>Windu & Siska</h1>
-            <p>17.01.2024 | Gianyar</p>
+            <p>14.04.2024 | Gianyar</p>
           </div>
         </div>
       </div>
@@ -94,9 +127,7 @@
     >
       <div class=" text-center p-10">
         <Saos
-          animation={isLoading
-            ? "scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-            : ""}
+          animation={"scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"}
         >
           <h1>Sang Mempelai</h1>
         </Saos>
@@ -128,7 +159,7 @@
             <p>Br.Sema, Desa Bitera, Gianyar</p>
           </Saos>
         </div>
-        <div>
+        <div class="py-5">
           <h1>&</h1>
         </div>
         <Saos
@@ -163,7 +194,7 @@
     </div>
   </section>
 
-  <section class="my-8 bg-slate-400">
+  <section class="my-8 bg-slate-100">
     <div class="p-5 text-center">
       <Saos
         animation={"scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"}
@@ -192,17 +223,21 @@
         >
           <div class="flex items-center justify-center h-[50vh] text-white">
             <div>
-              <img
-                src="https://undanganwebmedia.sgp1.digitaloceanspaces.com/withlove/hi/2022/02/icon.png"
-                class="w-[4rem] h-[4rem] mx-auto"
-                alt=""
-              />
-              <span class="block text-center py-5">Pawiwahan</span>
-              <hr />
-              <div class="py-5">
-                <p class="text-center">Rabu, 17 Januari 2024</p>
-                <p class="text-center">12.00 Wita</p>
-              </div>
+              <Saos
+                animation={"scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"}
+              >
+                <img
+                  src="https://undanganwebmedia.sgp1.digitaloceanspaces.com/withlove/hi/2022/02/icon.png"
+                  class="w-[4rem] h-[4rem] mx-auto"
+                  alt=""
+                />
+                <span class="block text-center py-5">Pawiwahan</span>
+                <hr />
+                <div class="py-5">
+                  <p class="text-center">Rabu, 17 Januari 2024</p>
+                  <p class="text-center">12.00 Wita</p>
+                </div>
+              </Saos>
             </div>
           </div>
         </div>
@@ -220,21 +255,108 @@
         >
           <div class="flex items-center justify-center h-[50vh] text-white">
             <div>
-              <img
-                src="https://undanganwebmedia.sgp1.digitaloceanspaces.com/withlove/hi/2022/02/icon.png"
-                class="w-[4rem] h-[4rem] mx-auto"
-                alt=""
-              />
-              <span class="block text-center py-5">Pawiwahan</span>
-              <hr />
-              <div class="py-5">
-                <p class="text-center">Rabu, 17 Januari 2024</p>
-                <p class="text-center">12.00 Wita</p>
+              <Saos
+                animation={"scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"}
+              >
+                <img
+                  src="https://undanganwebmedia.sgp1.digitaloceanspaces.com/withlove/hi/2022/02/icon.png"
+                  class="w-[4rem] h-[4rem] mx-auto"
+                  alt=""
+                />
+                <h1 class="block text-center py-5">Resepsi Nikah</h1>
+                <hr />
+
+                <div class="py-5">
+                  <p class="text-center">Minggu, 14 April 2024</p>
+                  <p class="text-center">14.00 Wita</p>
+                </div>
+                <hr />
+                <h1 class="block text-center py-3">Lokasi</h1>
+
+                <p class="text-center py-2">Br. Sema, Desa Bitera, Gianyar</p>
+
+                <button class=" bg-white text-black px-4 py-2 rounded-lg">
+                  Denah lokasi
+                </button>
+              </Saos>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="roundedshadow-lg bg-white m-5 rounded-lg">
+      <div class="p-5">
+        <img
+          class="w-full rounded-lg"
+          src="https://undanganwebmedia.sgp1.digitaloceanspaces.com/withlove/in/2024/01/PHOTO-EDITING-14.jpg"
+          alt="Sunset in the mountains"
+        />
+
+        <div class="px-6 py-4 text-center">
+          <div class="font-bold text-xl mb-2">
+            <h1 class="text-2xl">Menuju Hari Bahagia</h1>
+          </div>
+          <p class="text-gray-700 text-base">
+            Waktu berganti begitu cepat, dalam penantian kami yang penuh doa,
+            kami berharap kehadiran dan doa restu seluruh keluarga, sahabat dan
+            saudara terkasih, untuk menjadi saksi ikrar janji suci kami di hari
+            bahagia
+          </p>
+
+          <p>(Rgveda : X.85.42)</p>
+
+          <div class="py-5">
+            <div class="flex space-x-1 justify-center">
+              <div>
+                <h1>{daysLeft}</h1>
+                <span class="text-sm text-gray-600"> Days</span>
+              </div>
+              <div>
+                <h1>:</h1>
+              </div>
+              <div>
+                <h1>{hoursLeft}</h1>
+                <span class="text-sm text-gray-600">Hours</span>
+              </div>
+              <div>
+                <h1>:</h1>
+              </div>
+              <div>
+                <h1>{minutesLeft}</h1>
+                <span class="text-sm text-gray-600">Minutes</span>
+              </div>
+              <div>
+                <h1>:</h1>
+              </div>
+              <div>
+                <h1>{secondsLeft}</h1>
+                <span class="text-sm text-gray-600">Seconds</span>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="p-5 text-center">
+      <Saos
+        animation={"scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"}
+      >
+        <h1 class="p-10">Rsvp</h1>
+      </Saos>
+      <Saos
+        animation={"scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"}
+      >
+        <p>
+          Mohon konfirmasi kehadiran Bapak/Ibu/Saudara/i dengan mengisi form
+          berikut
+        </p>
+      </Saos>
     </div>
   </section>
 {/if}
